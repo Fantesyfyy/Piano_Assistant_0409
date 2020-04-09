3a588fae6bc07de558f0e56955aa1563cbce9641
Fantesy <229105187@qq.com>
2020-04-09 16:51:08 +0800

    04091651

---
+++ main.py
@@ -0,0 +1,108 @@
+import  pygame
+import time
+import mido #导入头文件
+from settings import *
+from pygame.locals import * #导入设置文件
+from midireader import  *
+
+#获取图片库和声音库路径
+img_dir = path.join(path.dirname(__file__),'pic')
+sound_folder = path.join(path.dirname(__file__),'sounds')
+
+pygame.display.set_caption("Piano Assitant")
+font_name = pygame.font.match_font('arial')
+
+
+sets=Set()
+pygame.init()
+screen = pygame.display.set_mode((sets.width,sets.height), DOUBLEBUF|HWSURFACE | FULLSCREEN, 32)
+screen.fill(sets.color) #创建窗口，编辑窗口数据
+clock=pygame.time.Clock()
+
+
+####加载图片###
+track0w_img= pygame.image.load(path.join(img_dir,'track0w.png')).convert()
+track0b_img= pygame.image.load(path.join(img_dir,'track0b.png')).convert()
+track1w_img= pygame.image.load(path.join(img_dir,'track1w.png')).convert()
+track1b_img= pygame.image.load(path.join(img_dir,'track1b.png')).convert()
+
+keyboard=keyboard_init() #加载键盘图库
+
+class Fallingnotes(pygame.sprite.Sprite):
+    '''创建掉落音符类'''
+    def __init__(self,track,note):
+        pygame.sprite.Sprite.__init__(self)
+        self.size = note.long/sets.falling_rate
+        self.track=track
+        self.tone=note.tone
+        self.velo=note.velo
+        self.time=note.time
+        self.long=note.long
+        self.worb=(note.tone%12)
+        if track==0:
+            if self.worb==0 :
+                self.image =track0b_img
+            else :
+                self.image = track0w_img
+        else :
+            if self.worb==0 :
+                self.image =track1b_img
+            else :
+                self.image = track1w_img
+        self.rect = self.image.get_rect()
+        self.rect.centerx =keyboard[self.tone][0].centerx
+        self.rect.bottom=0
+        self.last_update =frame
+
+    def update(self):
+        now = frame
+        self.rect.bottom+=(now - self.last_update)/sets.falling_rate
+        self.last_update = now
+        if self.rect.bottom-self.size>sets.height:
+            self.kill()
+
+def fps_control (fps):
+    clock.tick(fps)
+    getfps=clock.get_fps()
+    if getfps+0.5<sets.baseFPS:
+        fps+=0.1
+    elif getfps-0.5>sets.baseFPS:
+        fps-=0.1
+    print(getfps,fps)
+    return fps
+
+# 加载midi文件
+midiname='1.mid'
+mid = mido.MidiFile(path.join(sound_folder, midiname))  # 导入音乐mid文件
+perclick1 = (mid.tracks[0][3].clocks_per_click)
+melodys = readmidi(path.join(sound_folder, midiname))
+
+
+
+frame=0
+fps=sets.baseFPS
+note1=Fallingnotes(0,melodys[0][0])
+note2=Fallingnotes(0,melodys[0][1])
+notegroup=pygame.sprite.Group(note1)
+note2.add(notegroup)
+while True:
+    frame+=1
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            # 接收到退出事件后退出程序
+            exit()
+    screen.fill(sets.color)
+    notegroup.update()
+
+    notegroup.draw(screen)
+    for i in range(1,61):
+        if keyboard[i][1]==1:
+            pygame.draw.rect(screen, (255, 255, 240),keyboard[i][0],0)
+            pygame.draw.rect(screen, (0,0,0), keyboard[i][0], 1)
+    for i in range(1, 60):
+        if keyboard[i][1] ==0:
+            pygame.draw.rect(screen, (0,0,0), keyboard[i][0], 0)
+  # if melodys[0][0].time<=time.time()-starting:  # +修订值（半个循环时长）+(掉落时间)
+    pygame.display.flip()
+    fps=fps_control(fps)
+
---
+++ settings.py
@@ -0,0 +1,51 @@
+from os import path
+import  pygame
+import time
+import mido #导入头文件
+from pygame.locals import * #导入设置文件
+
+class Set:
+    def __init__(self):
+        self.width=1280
+        self.height=720
+        self.color=(54,52,51)
+        self.falling_rate=0.35  # 每多少帧向下落一像素
+        self.whitekeyheight=120 #键盘上的黑白键长宽
+        self.blackkeyheight=85
+        self.whitekeywidth=36
+        self.blackkeywidth=24
+        self.baseFPS=800
+sets=Set()
+
+class Note:
+    def __init__(self,tone,velo,time,long):
+        self.tone=tone
+        self.velo=velo
+        self.time=time  #按下时间
+        self.long=long  #时长
+        if self.tone%12 in  (1, 3, 5, 6, 8, 10, 0):
+            self.worb=1 #white/black black=0 white=1
+        else :
+            self.worb=0
+
+
+def keyboard_init():
+    keyboard = {}  # 字典，包含所有琴键的矩形对象
+    whitenum = 0
+    blacknum = 0  # 正在处理第几个黑/白键
+    black_position = -2-sets.whitekeywidth
+    # 正在处理黑键位置
+    for i in range(1, 61):
+        if i % 12 in (1, 3, 5, 6, 8, 10, 0):  # 是白键
+            whitenum += 1
+            keyboard[i] = (Rect(-26 + whitenum * sets.whitekeywidth, sets.height-sets.whitekeyheight, sets.whitekeywidth, sets.whitekeyheight), 1)  # 后面一个数black=0,white=1
+        else:
+            blacknum += 1
+            if blacknum % 5 in (3, 1):
+                black_position += sets.whitekeywidth*2
+            else:
+                black_position += sets.whitekeywidth
+            keyboard[i] = (Rect(black_position, sets.height-sets.whitekeyheight, sets.blackkeywidth, sets.blackkeyheight), 0)
+    return keyboard
+
+
