c071bff41c2e9d01e6937e55ac6d788a3cc9fb37
BugPlusCN <bugplus@outlook.com>
2020-04-19 14:25:30 +0800

    mmd git

--- main.py
+++ main.py
@@ -81,8 +81,6 @@ class Fallingnotes(pygame.sprite.Sprite):
         self.rect.bottom = 0
         self.floatbottom = 0
         self.last_update = frame
-<<<<<<< HEAD
-
     '''def update(self):
         now = frame
         self.floatbottom += (now - self.last_update)/sets.falling_rate
@@ -93,9 +91,6 @@ class Fallingnotes(pygame.sprite.Sprite):
         self.rect.bottom = 0
         self.floatbottom = 0
         self.last_update = frame'''
-
-=======
->>>>>>> 0c5167d541a3eeeebb2d4af4f3c9ee3622db1039
     def update(self):
         now = frame
         self.floatbottom += (now - self.last_update)/sets.falling_rate
